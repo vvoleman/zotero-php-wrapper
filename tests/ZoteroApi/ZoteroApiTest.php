859d8519d647cfe99e58858f4815f7614c91c8de
vvoleman <vojtech.voleman@tul.cz>
2022-03-28 11:46:58 +0200

    UPDATE MockResponse

--- tests/ZoteroApi/ZoteroApiTest.php
+++ tests/ZoteroApi/ZoteroApiTest.php
@@ -55,7 +55,7 @@ class ZoteroApiTest extends TestCase
     public function testGetBody(ZoteroApi $api)
     {
         $api->run();
-        $this->assertEquals("ABCDEFG",$api->getBody()["key"]);
+        $this->assertEquals("HH8ENUPI",$api->getBody()[0]["key"]);
         $this->assertIsArray($api->getBody(),"ZoteroApi::getBody() should return assoc array");
 
         $api = $this->getApiInstance();
